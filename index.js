6209e0f4c5971d36ebc1fc3dbe932119470b1b2c
Glitch (heliotrope-rattle-grandparent) <none>
2022-12-28 21:29:28 +0000

    🚘🌄 Checkpoint ./index.js:48062044/482

--- index.js
+++ index.js
@@ -133,8 +133,10 @@ message.channel.send(`${c.id} | ${c.name}`)
 let servers = " "; 
 let num = 0;
 client.guilds.cache.forEach(server =>{
+let membercountdcf = server.guild.memberCount;
+//server.members.cache.size;
 num = num + 1;
-servers += `= \`${num}\` ${server.name} | ${server.id} | ${server.guild.memberCount} \n `;
+servers += `= \`${num}\` ${server.name} | ${server.id} | ${membercountdcf} \n `;
 })
   //
 let embed = new Discord.MessageEmbed()
